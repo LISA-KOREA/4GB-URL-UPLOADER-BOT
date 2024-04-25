7a31a1a8d5c11483b931d55174170b62e74c96df
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2024-04-25 10:50:32 +0530

    Create configs.py

---
+++ Uploader/configs.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN","")
+API_ID = int(os.environ.get("API_ID", ))
+API_HASH = os.environ.get("API_HASH", "")
+DOWN_DIR = os.environ.get("DOWN_DIR", "./downloads")
+SESSION_STRING = os.environ.get("SESSION_STRING", "")
+
+
+
