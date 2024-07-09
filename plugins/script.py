b7ddfde82e823c85d49a0272198d5276dc644953
Lisa <106958298+LISA-KOREA@users.noreply.github.com>
2024-07-09 16:04:58 +0530

    Update script.py

--- plugins/script.py
+++ plugins/script.py
@@ -1,3 +1,12 @@
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 class Translation(object):
+
+  START_TEXT = """ Hello bro donete me tg premium to start
+
+Contact me @LISA_FAN_LK
+"""
+  HELP_TEXT = """ donete tg premium 
+
+  """
+  
